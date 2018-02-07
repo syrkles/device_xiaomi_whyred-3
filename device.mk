ec2ee16099cfe8b8589558b4cebf4f740611be65
Bruno Martins <bgcngm@gmail.com>
2018-02-07 00:41:09 +0000

    whyred: Use MIUI poweroff charging animation
     * It's cool, and more importantly... it actually works!
       Oh look, it even displays battery level. Wow. :-P
    
    Change-Id: I6351e2eac6df89aa1b6b98e4e6b2815fa5c37794
    Signed-off-by: Subhaam <derksoul98@gmail.com>
    Signed-off-by: Shahan-mik3 <mik3dev93@gmail.com>

--- device.mk
+++ device.mk
@@ -408,11 +408,11 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
-    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
+# Healthd
+PRODUCT_PACKAGES += \
+    chargeonlymode
 
-# Tetheroffload
+# HIDL
 PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml \
