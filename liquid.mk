40c0964dafe205f1c26c37e216fd90d7173c0cb9
syrkles <syrkles@gmail.com>
2018-08-04 23:00:24 +0000

    Liquid bring up

--- liquid.mk
+++ liquid.mk
@@ -21,13 +21,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from whyred device
 $(call inherit-product, device/xiaomi/whyred/device.mk)
 
-# Inherit some common Lineage OS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Liquid stuff.
+$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 TARGET_VENDOR := Xiaomi
 PRODUCT_DEVICE := whyred
-PRODUCT_NAME := lineage_whyred
+PRODUCT_NAME := liquid_whyred
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 BOARD_VENDOR := Xiaomi
@@ -41,5 +41,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := xiaomi/whyred/whyred:8.1.0/OPM1.171019.011/8.6.14:user/release-keys
 
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
-    ro.product.model
